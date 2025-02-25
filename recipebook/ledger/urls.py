2da3370cb79b07429a19039a54f043c0fde6343d
chrisxdale <chrisxdalelopez@gmail.com>
2025-02-25 22:52:13 +0800

    Updated urls.py under ledger
    Linked the path for the three recipe pages in the website.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, recipes_list, recipe_one, recipe_two
 
 urlpatterns = [
     path('', index, name='index'),
+    path('recipes/list', recipes_list, name='recipes-list'),
+    path('recipes/1', recipe_one, name='recipe-one'),
+    path('recipes/2', recipe_two, name='recipe-two'),
 ]
 
 app_name = "ledger"
