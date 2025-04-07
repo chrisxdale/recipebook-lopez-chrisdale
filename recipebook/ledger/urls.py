b6d6592110d2dde824abca60c3d33b4f35a639d5
chrisxdale <chrisxdalelopez@gmail.com>
2025-04-07 20:08:01 +0800

    Fixed urls.py under ledger app
    Changed the path to a more general one instead of the hardcoded

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import index, recipes_list, recipe_one, recipe_two
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('recipes/list', recipes_list, name='recipes-list'),
-    path('recipe/1', recipe_one, name='recipe-one'),
-    path('recipe/2', recipe_two, name='recipe-two'),
+    path('recipes/list', views.recipes_list, name='recipes-list'),
+    path('recipe/<int:id>', views.recipe_details, name='recipe-details'),
 ]
 
 APP_NAME = "ledger"
