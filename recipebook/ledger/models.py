da1bf92199431ba6a9ca0cd9c8759524d6fe3187
chrisxdale <chrisxdalelopez@gmail.com>
2025-04-07 20:02:02 +0800

    Fixed importing error by importing reverse
    Imported reverse from django.urls

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,22 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ingredient_detail', arg=[str(self.name)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    
+
     def __str__(self):
-         return self.name
-     
+        return self.name
+
     def get_absolute_url(self):
-         return reverse('recipesInDatabase')
+        return reverse('recipesInDatabase')
 
 class RecipeIngredient(models.Model):
     Quantity = models.CharField(max_length=100)
