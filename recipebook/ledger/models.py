7849b3f0189297680bbe4efa3cb8aa0e0a248ca9
chrisxdale <chrisxdalelopez@gmail.com>
2025-04-07 21:33:27 +0800

    Created Profile model
    Created profile model, added author to the Ingredient model which includes the profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
     '''     
@@ -20,6 +29,14 @@ class Recipe(models.Model):
     @brief The model class for Recipe
     '''
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    created_on = models.DateTimeField(auto_now_add=True),
+    updated_on = models.DateTimeField(auto_now=True),
 
     def __str__(self):
         return self.name
