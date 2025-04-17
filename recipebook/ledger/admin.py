c18c1896b9b8b7c4096fef89fec509e4cf977fc7
chrisxdale <chrisxdalelopez@gmail.com>
2025-04-17 23:38:37 +0800

    Updated admin.py to include Profile Model admin
    Included ProfileInline class and UserAdmin class

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Ingredient, Recipe, RecipeIngredient, Profile
 
 class IngredientAdmin(admin.ModelAdmin):
     '''     
-    @fn IngredientAdmin
+    @cn IngredientAdmin
     @brief The admin class for Ingredient
     '''
     model = Ingredient
@@ -12,7 +14,7 @@ class IngredientAdmin(admin.ModelAdmin):
 
 class RecipeAdmin(admin.ModelAdmin):
     '''     
-    @fn RecipeAdmin
+    @cn RecipeAdmin
     @brief The admin class for Recipe
     '''
     model = Recipe
@@ -21,13 +23,30 @@ class RecipeAdmin(admin.ModelAdmin):
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     '''     
-    @fn RecipeIngredientAdmin
+    @cn RecipeIngredientAdmin
     @brief The admin class for RecipeIngredient
     '''
     model = RecipeIngredient
 
     list_display = ('Quantity', 'Ingredient', 'Recipe',)
 
+class ProfileInline(admin.StackedInline):
+    '''     
+    @cn ProfileInline
+    @brief Contains the Profile Model which to be used in UserAdmin
+    '''
+    model = Profile
+    can_delete = False
+
+class UserAdmin(admin.BaseUserAdmin):
+    '''     
+    @cn UserAdmin
+    @brief The admin class for User
+    '''
+    inlines = [ProfileInline,]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
