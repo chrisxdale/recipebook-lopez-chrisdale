c2a01dfdafec8cd04d19605adb3778ae66eaa441
chrisxdale <chrisxdalelopez@gmail.com>
2025-05-01 22:46:16 +0800

    Updated views.py to include all forms
    Added form views for RecipeCreateView, RecipeIngredientCreateView, IngredientCreateView, and RecipeImageCreateView.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Recipe
+from .models import Recipe, RecipeImage, RecipeIngredient, Ingredient
+from .forms import RecipeForm, RecipeImageForm, RecipeIngredientForm, IngredientForm
+from django.urls import reverse_lazy
 
 class RecipeListView(LoginRequiredMixin, ListView):
     '''     
@@ -21,3 +23,70 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'recipe'
     model = Recipe
     template_name = 'recipe.html'
+    extra_context = {"images":RecipeImage.objects.all}
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    '''     
+    @cn RecipeCreateView
+    @brief Contains the form view for adding recipes
+    '''
+    model = Recipe
+    template_name = "recipe_form.html"
+    form_class = RecipeForm
+    success_url = "/recipe/add/recipeingredient/"
+ 
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+ 
+    def get_context_data(self, **kwargs):
+        context = super(RecipeCreateView, self).get_context_data(**kwargs)
+        context['recipe_form'] = context['form']
+        return context
+
+class RecipeIngredientCreateView(LoginRequiredMixin, CreateView):
+    '''     
+    @cn RecipeIngredientCreateView
+    @brief Contains the form view for adding ingredient or recipe ingredient
+    '''
+    model = RecipeIngredient
+    template_name = "recipeingredient_form.html"
+    form_class = RecipeIngredientForm
+    success_url = reverse_lazy('ledger:recipes-list')
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeIngredientCreateView, self).get_context_data(**kwargs)
+        context['recipeingredient_form'] = context['form']
+        return context
+
+class IngredientCreateView(LoginRequiredMixin, CreateView):
+    '''     
+    @cn IngredientCreateView
+    @brief Contains the form view for adding ingredient
+    '''
+    model = Ingredient
+    template_name = "ingredient_form.html"
+    form_class = IngredientForm
+    success_url = "/recipe/add/recipeingredient/"
+
+    def get_context_data(self, **kwargs):
+        context = super(IngredientCreateView, self).get_context_data(**kwargs)
+        context['ingredient_form'] = context['form']
+        return context
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    '''     
+    @cn RecipeImageCreateView
+    @brief Contains the form view for adding recipe images
+    '''
+    model = RecipeImage
+    template_name = "recipeimage_form.html"
+    form_class = RecipeImageForm
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe-detail', kwargs={ 'pk': self.object.recipe.pk })
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeImageCreateView, self).get_context_data(**kwargs)
+        context['recipeimage_form'] = context['form']
+        return context
